1091f779f440052ac97d33d8d29500328641e618
vothanhtong <vothanhtong9295@gmail.com>
2025-03-26 21:12:01 +0700

    add

--- 100_bai_tap.py
+++ 100_bai_tap.py
@@ -304,7 +304,7 @@ while s<a:
     k+=1
 print(k-2)
 # BÀI  36:Nhập vào A, tìm n nhỏ nhất sao cho
-1 + 1/2 + 1/3 + 1/4 + ... + 1/n > A
+# 1 + 1/2 + 1/3 + 1/4 + ... + 1/n > A
 a=float(input("nhập vào a: "))  # đây là số thực 
 s=0
 k=1
