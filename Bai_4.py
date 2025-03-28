a3a88e6c10b04e3762525a3f0086f80a0ca8be8d
vothanhtong <vothanhtong9295@gmail.com>
2025-03-28 09:00:09 +0700

    add update bt

--- Bai_4.py
+++ Bai_4.py
@@ -4,3 +4,13 @@ if a%2==0:
     print("TRUE")
 else:
     print("FALSE")
+
+# Bài 4.1 nhập vào số nguyên a chia hết cho 3 thì in ra True, ngược lại in ra Fale 
+a = int(input("nhập số nguyên a :"))
+if a % 3 == 0:
+    print("TRUE")
+else:
+    print("Fale")
+
+# Bài 4.2 nhập số nguyên a chia hết cho 5 
+a = int(input("nhập số bguyeen bất kỳ"))
