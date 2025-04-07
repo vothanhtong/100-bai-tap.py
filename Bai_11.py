7811fb4e18d2412668745d87dce9944487a283cd
vothanhtong <vothanhtong9295@gmail.com>
2025-04-07 07:23:21 +0700

    add update

--- Bai_11.py
+++ Bai_11.py
@@ -1,9 +1,9 @@
-# # BÀI 11:Nhập vào 3 số a, b, c. In ra kết quả là tổng của ba số đó
-# a=int(input("nhập vào số nguyên a: "))
-# b=int(input("nhập vào số nguyên b: "))
-# c=int(input("nhập vào số nguyên c: "))
-# tong=a+b+c
-# print("tổng ba số a,b,a là: ",tong)
+# BÀI 11:Nhập vào 3 số a, b, c. In ra kết quả là tổng của ba số đó
+a=int(input("nhập vào số nguyên a: "))
+b=int(input("nhập vào số nguyên b: "))
+c=int(input("nhập vào số nguyên c: "))
+tong=a+b+c
+print("tổng ba số a,b,a là: ",tong)
 
 ## BÀI: In bảng cửu chương không trùng lặp
 def print_multiplication_table():
--- Bai_30.py
+++ Bai_30.py
@@ -5,3 +5,18 @@ for i in range(1,a+1):
     if a%i==0:
         dem += 1
 print(dem)
+
+# BÀI 31: Nhập vào số nguyên dương a, kiểm tra xem a có phải là số nguyên tố hay không
+a = int(input("Nhập vào số nguyên dương a: "))
+if a < 2:
+    print("Không phải là số nguyên tố")
+else:
+    is_prime = True
+    for i in range(2, int(a**0.5) + 1):  # Kiểm tra từ 2 đến căn bậc hai của a
+        if a % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print("Đây là số nguyên tố")
+    else:
+        print("Không phải là số nguyên tố")
