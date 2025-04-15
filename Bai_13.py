c03f1492a8bfdf465f490b46933f23646bd4618c
vothanhtong <vothanhtong9295@gmail.com>
2025-04-15 13:56:59 +0700

    add update

--- Bai_13.py
+++ Bai_13.py
@@ -1,7 +1,31 @@
 # BÀI TẬP LỆNH ĐIỀU KIỆN CƠ BẢN
-# BÀI 13:Nhập vào số nguyên dương a, nếu a lớn hơn 10 thì ta in ra đây là số lớn hơn 10
-a=int(input("nhập vào số nguyên a: "))
-if a>10 :
-    print("đây là số lớn hơn 10: ")
+
+# BÀI 13: Nhập vào số nguyên dương a, nếu a lớn hơn 10 thì in ra đây là số lớn hơn 10
+a = int(input("Nhập vào số nguyên a: "))
+if a > 10:
+    print("Đây là số lớn hơn 10.")
 else:
-    print("đây là số nhỏ hơn 10: ")
+    print("Đây là số nhỏ hơn hoặc bằng 10.")
+
+# BÀI 14: Kiểm tra số chẵn hay lẻ
+b = int(input("Nhập vào số nguyên b: "))
+if b % 2 == 0:
+    print("Đây là số chẵn.")
+else:
+    print("Đây là số lẻ.")
+
+# BÀI 15: Kiểm tra số âm, dương hay bằng 0
+c = int(input("Nhập vào số nguyên c: "))
+if c > 0:
+    print("Đây là số dương.")
+elif c < 0:
+    print("Đây là số âm.")
+else:
+    print("Đây là số 0.")
+
+# BÀI 16: Kiểm tra số nguyên d có chia hết cho 5 hay không
+d = int(input("Nhập vào số nguyên d: "))
+if d % 5 == 0:
+    print("Số này chia hết cho 5.")
+else:
+    print("Số này không chia hết cho 5.")
---
+++ Basic-varible-insertion.py
@@ -0,0 +1,32 @@
+# 1basic variable insertion
+name = "Clcoding"
+print(f"hello, {name}")
+# 2basic variable insertion
+x,y=5,10
+print(f"hello, {x} and {y}")
+# 3basic variable insertion
+price = 100
+quantity = 5
+total = price * quantity
+print(f"Total price is {total}")
+# 4basic variable insertion
+x = 5
+y = 10
+z = 15
+print(f"x: {x}, y: {y}, z: {z}")
+# 5basic variable insertion
+x = 5
+y = 10
+z = 15
+print(f"x: {x}, y: {y}, z: {z}")
+# 6number formatting
+x = 5
+y = 10
+z = 15  
+print(f"x: {x}, y: {y}, z: {z}")
+# 7debugging with = 
+x = 5
+print(f"x: {x}")
+# 8 use wwhit dictionaries
+person = {"name": "Alice", "age": 30}
+print(f"Name: {person['name']}, Age: {person['age']}")
